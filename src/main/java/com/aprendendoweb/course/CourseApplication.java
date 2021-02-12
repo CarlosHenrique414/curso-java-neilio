edeb44663826993bedba63ec05dbb185e31d001d
CarlosHenrique414 <xideiafizzx@gmail.com>
2021-02-12 20:13:33 -0300

    Agora eu aprendi

--- src/main/java/com/aprendendoweb/course/CourseApplication.java
+++ src/main/java/com/aprendendoweb/course/CourseApplication.java
@@ -9,7 +9,7 @@ public class CourseApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CourseApplication.class, args);
 		
-		System.out.println("uma alteração qualquer para testar o github presta atenção cabaço");
+		System.out.println("Agora eu aprendi!!!");
 	}
 
 }
