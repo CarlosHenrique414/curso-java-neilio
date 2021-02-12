345effe2ee1956cf6df6bbae8e134dceb624535f
CarlosHenrique414 <xideiafizzx@gmail.com>
2021-02-12 20:10:13 -0300

    uma alteração qualquer para testar o github presta atenção cabaço

--- src/main/java/com/aprendendoweb/course/CourseApplication.java
+++ src/main/java/com/aprendendoweb/course/CourseApplication.java
@@ -8,6 +8,8 @@ public class CourseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CourseApplication.class, args);
+		
+		System.out.println("uma alteração qualquer para testar o github presta atenção cabaço");
 	}
 
 }
