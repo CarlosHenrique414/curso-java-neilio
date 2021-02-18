9f436217bf0c250197b9afaf22107109dfb767d2
CarlosHenrique414 <xideiafizzx@gmail.com>
2021-02-18 20:52:21 -0300

    Product

--- src/main/java/com/aprendendoweb/course/entities/Product.java
+++ src/main/java/com/aprendendoweb/course/entities/Product.java
@@ -30,7 +30,7 @@ public class Product implements Serializable {
 	
 	public Product() {
 	}
-	public Product(Long id, String name, String description, Double price, String imgUrl, Set<Category> categories) {
+	public Product(Long id, String name, String description, Double price, String imgUrl) {
 		super();
 		this.id = id;
 		this.name = name;
